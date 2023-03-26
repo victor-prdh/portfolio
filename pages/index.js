a3703c50db7ac39abcc0ccbb976f128e4f1f4b3d
victor-prdh <victor.prudhomme60600@gmail.com>
2023-03-26 18:53:46 +0200

    feat: homepage

--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+import Button from '../components/button';
 import CustomHead from '../components/head';
 import Header from '../components/header';
 import styles from '../styles/home.module.css';
@@ -8,7 +10,14 @@ export default function Home() {
       <CustomHead />
       <Header></Header>
       <div className={styles.presentation}>
-        <h1>Salut</h1>
+        <h1>Développeur passioné par la création de solutions modernes et performantes</h1>
+        <p>Développeur back chez&nbsp;
+          <Link href={'https://www.sinfin.fr'} target="_blank" className={styles.link}>Sinfin</Link>.
+          Grand utilisateur de Symfony et React.
+        </p>
+        <Link href={"/about"} className={styles.btnContainer}>
+          <Button title={"Me découvrir"} />
+        </Link>
       </div>
     </>
   )
