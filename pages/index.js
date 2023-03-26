7bab3652d4581b7e892fc3eae447d498356b4f0b
victor-prdh <victor.prudhomme60600@gmail.com>
2023-03-26 18:19:02 +0200

    feat: add custom head

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,11 @@
+import CustomHead from '../components/head';
 import Header from '../components/header';
 import styles from '../styles/home.module.css';
 
 export default function Home() {
   return (
     <>
+      <CustomHead />
       <Header></Header>
       <div className={styles.presentation}>
         <h1>Salut</h1>
