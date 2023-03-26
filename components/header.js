457f107c86a7d3e02f06fb09bcfecde8f7fe98e6
victor-prdh <victor.prudhomme60600@gmail.com>
2023-03-26 18:02:33 +0200

    feat: add link to title

--- components/header.js
+++ components/header.js
@@ -5,14 +5,16 @@ import Link from 'next/link'
 export default function Header() {
   return (
     <header className={styles.header}>
+      <Link href='/'>
         <span className={styles.title}>Victor&nbsp;Prudhomme</span>
+      </Link>
 
-        <nav>
-            <ul className={styles.navigation}>
-                <li><Link href="/">home</Link></li>
-                <li><Link href="/about">à&nbsp;propos</Link></li>
-            </ul>
-        </nav>
+      <nav>
+        <ul className={styles.navigation}>
+          <li><Link href="/">home</Link></li>
+          <li><Link href="/about">à&nbsp;propos</Link></li>
+        </ul>
+      </nav>
     </header>
   )
 }
