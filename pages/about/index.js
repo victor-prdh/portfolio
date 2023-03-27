976b8b00dbf9697174a48e13d7c07fef749e26a0
victor-prdh <victor.prudhomme60600@gmail.com>
2023-03-27 20:48:01 +0200

    feat: start about page

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,6 +1,8 @@
 import CustomHead from '../../components/head';
 import Header from '../../components/header';
-import styles from '../../styles/home.module.css';
+import styles from '../../styles/about.module.css';
+import me from '../../public/me.jpg'
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -8,7 +10,12 @@ export default function Home() {
       <CustomHead title={"À propos - Victor Prudhomme"} />
       <Header></Header>
       <div className={styles.presentation}>
-        <h1>ABOUT</h1>
+        <Image src={me} width={312} height={312} className={styles.img} />
+        <div>
+          <h1>Hello !</h1>
+          <p>Moi c'est Victor, un développeur passionné par la 
+            création d'applications performantes tout en ayant la volonté de m'améliorer continuellement.</p>
+        </div>
       </div>
     </>
   )
